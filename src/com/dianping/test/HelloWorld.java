8391ce6f0e49fce34430b5a52d49432d14117610
luomeiling <974474929@qq.com>
2016-09-07 13:31:57 +0800

    提交试试

---
+++ src/com/dianping/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.dianping.test;
+
+/**
+ * Created by luomeiling on 16/9/7.
+ */
+public class HelloWorld {
+    public static void main(String []args){
+    System.out.print("Hello World");
+    }
+}
